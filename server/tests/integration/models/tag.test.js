d1a72b651289a82cfcd37631a8d9ac3d7fdc1603
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2023-12-07 12:17:55 -0800

    Implement Tag schema and moel with unit and integration tests

---
+++ server/tests/integration/models/tag.test.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Tag } from '../../../models/tag';
+
+let mongoServer;
+
+// Set up
+beforeAll(async () => {
+	mongoServer = await MongoMemoryServer.create();
+	const uri = mongoServer.getUri();
+	await mongoose.connect(uri);
+});
+
+// Clear the collection after each test
+afterEach(async () => {
+	await mongoose.connection.dropDatabase();
+});
+
+// Teardown
+afterAll(async () => {
+	await mongoose.disconnect();
+	await mongoServer.stop();
+});
+
+describe('Tag Model Tests', () => {
+	// Create and save a new tag successfully
+	it('should create and save a new tag successfully', async () => {
+		const tagData = {
+			name: 'Test Tag',
+			isPredefined: false,
+		};
+
+		const validTag = new Tag(tagData);
+		const savedTag = await validTag.save();
+
+		expect(savedTag._id).toBeDefined();
+		expect(savedTag.name).toBe(tagData.name);
+		expect(savedTag.isPredefined).toBe(tagData.isPredefined);
+	});
+
+	// Update tag
+	it('should update a tag and have the changes reflected in the database', async () => {
+		const tag = await new Tag({
+			name: 'Test Tag',
+			isPredefined: false,
+		}).save();
+
+		// Store the original name before updating
+		const originalName = tag.name;
+		tag.name = 'Updated Name';
+
+		// Save the updated tag and retrieve it to confirm update
+		const savedTag = await tag.save();
+		const updatedTag = await Tag.findById(savedTag._id);
+
+		expect(updatedTag.name).toBe('Updated Name');
+		expect(updatedTag.name).not.toBe(originalName);
+	});
+
+	// Delete tag
+	it('should remove a tag from the database', async () => {
+		const tag = await new Tag({
+			name: 'Test Tag',
+			isPredefined: false,
+		}).save();
+
+		const tagId = tag._id;
+		await Tag.findByIdAndDelete(tagId);
+
+		const deletedTag = await Tag.findById(tagId);
+		expect(deletedTag).toBeNull();
+	});
+});
---
+++ server/tests/unit/models/tag.test.js
@@ -0,0 +1,80 @@
+import { validateTag } from '../../../models/tag';
+
+describe('Tag Model Validation Tests', () => {
+	// Valid tag
+	it('should pass with valid tag data', () => {
+		const tagData = {
+			name: 'Test Tag',
+			isPredefined: false,
+		};
+		const { error } = validateTag(tagData);
+
+		expect(error).toBeUndefined();
+	});
+
+	// Missing name field
+	it('should report an error if name is missing', () => {
+		const tagData = {
+			isPredefined: false,
+		};
+		const { error } = validateTag(tagData);
+
+		expect(error).toBeDefined();
+		expect(error.details[0].message).toContain('"name" is required');
+	});
+
+	// Empty name field
+	it('should report an error if the name is empty', () => {
+		const tagData = {
+			name: '',
+			isPredefined: false,
+		};
+		const { error } = validateTag(tagData);
+
+		expect(error).toBeDefined();
+		expect(error.details[0].message).toContain(
+			'"name" is not allowed to be empty'
+		);
+	});
+
+	// Name is too long
+	it('should report an error if the name is too long', () => {
+		const tagData = {
+			name: '12345678912345678912345',
+			isPredefined: false,
+		};
+		const { error } = validateTag(tagData);
+
+		expect(error).toBeDefined();
+		expect(error.details[0].message).toContain(
+			'"name" length must be less than or equal to 20 characters long'
+		);
+	});
+
+	// Missing isPredefined field
+	it('should report an error if the isPredefined field is missing', () => {
+		const tagData = {
+			name: 'Test Tag',
+		};
+		const { error } = validateTag(tagData);
+
+		expect(error).toBeDefined();
+		expect(error.details[0].message).toContain(
+			'"isPredefined" is required'
+		);
+	});
+
+	// Wrong type in isPredefined field
+	it('should report an error if the isPredefined field is empty', () => {
+		const tagData = {
+			name: 'Test Tag',
+			isPredefined: 'false',
+		};
+		const { error } = validateTag(tagData);
+
+		expect(error).toBeDefined();
+		expect(error.details[0].message).toContain(
+			'"isPredefined" must be a boolean'
+		);
+	});
+});
