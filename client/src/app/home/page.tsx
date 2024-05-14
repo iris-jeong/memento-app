81290acd8f8c9e3a3a19b0ddb671aedcb15d8cff
Iris Jeong <77691556+iris-jeong@users.noreply.github.com>
2024-05-14 13:21:45 -0700

    Add loading spinner to home page

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { BeatLoader } from 'react-spinners';
 import { EntryType } from '@/types/entries';
 import { useAuth } from '@/hooks/useAuth';
 import { getEntries } from '@/api/entries';
@@ -8,16 +9,17 @@ import Header from '@/components/organisms/Header';
 import EntryForm from '@/components/organisms/EntryForm';
 import AllEntries from '@/components/organisms/AllEntries';
 import Toast from '@/components/atoms/Toast';
+import { useAuthRedirect } from '@/hooks/useAuthRedirect';
 
 export default function Home() {
 	const { isAuthenticated, token } = useAuth();
 	const router = useRouter();
+	const { isLoading, isRedirecting } = useAuthRedirect('/login');
 
 	const [entries, setEntries] = useState<EntryType[]>([]);
 	const [highlightedEntryId, setHighlightedEntryId] = useState<string | null>(
 		null
 	);
-	const [isLoading, setIsLoading] = useState(true);
 
 	const highlightEntry = (entryId: string) => {
 		setHighlightedEntryId(entryId);
@@ -34,21 +36,28 @@ export default function Home() {
 		}
 
 		const fetchEntries = async () => {
-			setIsLoading(true);
 			try {
 				const data = await getEntries(token);
 				setEntries(data);
 			} catch (error) {
 				console.error('Error fetching data: ', error);
-			} finally {
-				setIsLoading(false);
 			}
 		};
 
 		fetchEntries();
 	}, [router, isAuthenticated]);
 
-	if (isLoading) return <div>Loading...</div>;
+	if (isLoading || isRedirecting) {
+		return (
+			<div className="flex justify-center items-center h-screen">
+				<BeatLoader
+					loading={true}
+					color="#1945E2"
+					aria-label="Loading Spinner"
+				/>
+			</div>
+		);
+	}
 
 	return (
 		<div className="relative">
